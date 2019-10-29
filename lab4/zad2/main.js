d67d4d07a0ae961fdbf3e5358f24a85d969b8c5b
Krzysztof Czyż <krzys6221@gmail.com>
2019-10-29 16:46:03 +0100

    lab4

---
+++ lab4/zad2/main.js
@@ -0,0 +1,20 @@
+
+el1 = document.getElementById("Pierwszy");
+addEventListener(el1,multiplyBy);
+el1 = document.getElementById("Drugi");
+addEventListener(el1,divideBy);
+
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
---
+++ lab4/zad3/main.js
@@ -0,0 +1,17 @@
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    alert(message);
+}
+
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    alert(message);
+} 
+cToF(60);
+fToC(45);
