a9fa7d584d7fea46d7e7836fc6a9715e267026b0
Karol Majer <ktoczytamaila@tenmaraka.pl>
2019-11-04 15:26:03 +0100

    Id nie były potrzebne

---
+++ lab05/main.js
@@ -0,0 +1,54 @@
+function $(id)
+{
+    return document.getElementById(id);
+}
+
+document.body.onload = ()=>
+{
+    $("button1").addEventListener("click",()=>
+    {
+        $("p1").style.color = "red";
+        $("p1").style.fontSize = "18px";
+        $("p1").style.fontFamily = "Verdana";
+    });
+
+    $("button2").addEventListener("click",()=>
+    {
+        $("img2").src = $("input2").value; 
+    });
+
+    $("button3").addEventListener("click",()=>
+    {
+        $("ul3").innerHTML = `<li><a href="${$("input3_2").value}">${$("input3_1").value}</a></li>${$("ul3").innerHTML}` 
+    });
+
+    $("div4").addEventListener("mouseenter",()=>
+    {
+        var d = new Date();
+        $("div4").innerHTML = `${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()}`;
+    });
+
+    $("div4").addEventListener("mouseleave",()=>
+    {
+        $("div4").innerHTML = "Dowolny tekst";
+    });
+
+    $("div4").addEventListener("click",()=>
+    {
+        $("span4").innerHTML = $("div4").innerHTML;
+    });
+
+    var buttons5 = Array.from(document.getElementsByClassName("button5"));
+    console.log(buttons5);
+    buttons5.forEach(elem=>elem.addEventListener("click",(e)=>
+    {
+        buttons5.forEach(elem1=>
+        {
+            elem1.style.fontWeight = "normal";
+            elem1.style.background = "#EEEEEE";
+        });
+        e.srcElement.style.fontWeight = "bold";
+        e.srcElement.style.backgroundColor = "#CCFFFF";
+    }));
+}
+
