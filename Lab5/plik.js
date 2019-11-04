14d68dce20a410de12de2e04409dbf3fabd8e2bb
Mateusz_S <mateuszszczesny@o2.pl>
2019-11-04 17:29:10 +0100

    lab5

---
+++ Lab5/plik.js
@@ -0,0 +1,16 @@
+function Zad1() {
+    document.getElementById("pierwsze").style.color = "red";
+    document.getElementById("pierwsze").style.fontSize = "35px";
+    document.getElementById("pierwsze").style.fontFamily = "Verdana";
+    }
+function Zad2(){
+    var element = document.getElementById("drugieA");
+    element.innerHTML = "Nowy tekst";
+}
+function Zad3(){
+    var para = document.createElement("p");
+    var node = document.createTextNode("This is new.").para.appendChild(node);
+    var element = document.getElementById("div1");
+    element.appendChild(para);
+}
+    
