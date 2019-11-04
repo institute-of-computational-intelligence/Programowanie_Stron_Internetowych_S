d342682571718b89247ef5c6f43486a226ce5ce5
Michał <michal@abc>
2019-11-04 17:33:35 +0100

    Lab 5

--- Lab 4/scripts.js
+++ Lab 4/scripts.js
@@ -47,31 +47,31 @@ function silnia(){
     document.getElementById("result4").innerHTML = d;
 }
 
-const arr1=[-3,8,7,6,5,-4,3,2,1];
-const arr2=[];
-let min=arr1[0];
+const tab1=[-3,8,7,6,5,-4,3,2,1];
+const tab2=[];
+let min=tab1[0];
 let pos;
-var max=arr1[0];
-for (i=0; i<arr1.length; i++)
+var max=tab1[0];
+for (i=0; i<tab1.length; i++)
 {
-        if (max<arr1[i]) max=arr1[i];
+        if (max<tab1[i]) max=tab1[i];
 }
 
-for (var i=0;i<arr1.length;i++)
+for (var i=0;i<tab1.length;i++)
 {
-        for (let j=0;j<arr1.length;j++)
+        for (let j=0;j<tab1.length;j++)
         {
-                if (arr1[j]!="x")
+                if (tab1[j]!="x")
                 {
-                        if (min>arr1[j]) 
+                        if (min>tab1[j]) 
                         {
-                                min=arr1[j];
+                                min=tab1[j];
                                 pos=j;
                         }
                 }
         }
-        arr2[i]=min;
-        arr1[pos]="x";
+        tab2[i]=min;
+        tab1[pos]="x";
         min=max;
 }
-console.log(arr2);
+console.log(tab2);
---
+++ Lab 5/script.js
@@ -0,0 +1,40 @@
+function kolor(){
+    document.getElementById("p2").style.color = "blue";
+    document.getElementById("p2").style.fontSize="20px";
+    document.getElementById("p2").style.fontFamily="Sans-serif";
+}
+
+function obraz(){
+    sorce=document.getElementById("sorce").value;
+    document.getElementById("zdjecie").src = sorce;
+}
+
+function odn(){
+    var lii=document.createElement("li");
+    var para = document.createElement("a");
+    tekst=document.getElementById("t1").value;
+    para.setAttribute("href",document.getElementById("t2").value)
+    var node = document.createTextNode(tekst);
+    para.appendChild(node);
+    lii.appendChild(para);
+    var element = document.getElementById("ul");
+    element.insertBefore(lii, element.childNodes[0]);
+}
+function mOver(obj) {
+    obj.innerHTML = Date();
+}
+function mOut(obj) {
+    obj.innerHTML = "tekst"
+  }
+function klikniecie(obj) {
+    t1=obj.innerHTML;
+    document.getElementById("zad4.1").innerHTML=t1;
+}
+function mOver1(obj) {
+    obj.style.fontSize="20px";
+    obj.style.background="blue";
+}
+function mOut1(obj) {
+    obj.style.fontSize="14px"
+    obj.style.background="gray";
+  }
