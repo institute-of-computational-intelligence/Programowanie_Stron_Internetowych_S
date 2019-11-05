f7e1b1a564afe8e61475552d3c17d3ecb8d7f240
Łukasz_Kowalik <lukasz.kowalik41st@gmail.com>
2019-11-05 16:27:14 +0100

    added zad1

---
+++ lab5/script.js
@@ -0,0 +1,11 @@
+//*zad1
+document.querySelector('#btn-1').addEventListener('click', () => {
+    const p = document.getElementById('zad1');
+    p.style.fontWeight = 'bold';
+    p.style.fontSize = '20px';
+    p.style.fontFamily = 'Arial';
+    p.style.color = 'red';
+})
+//*zad2
+
+//*zad3
