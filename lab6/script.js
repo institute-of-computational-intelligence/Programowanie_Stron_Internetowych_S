cd3fb25a183642962cd48f6362d04b728efefaad
Łukasz_Kowalik <lukasz.kowalik41st@gmail.com>
2019-11-12 17:40:00 +0100

    added zad4

--- lab6/script.js
+++ lab6/script.js
@@ -4,3 +4,16 @@ $('#zmienKolor').click(() => {
     $('#component').children('div').css('color', 'blue');
     $('#component').children('.orange').css('color', 'orange');
 })
+//*zad 4
+$('#addToFront').click(() => {
+    const i = $('#zad4').children('input').val();
+    $('#zad4').children('ul')
+        .prepend('<li> ' +
+            i + '</li>');
+});
+$('#addToEnd').click(() => {
+    const i = $('#zad4').children('input').val();
+    $('#zad4').children('ul')
+        .append('<li> ' +
+            i + '</li>');
+});
