2ae73aece79e82bf85ab9eb525adbfda6013596d
Krzysztof Czyż <krzys6221@gmail.com>
2019-12-03 17:05:18 +0100

    Poczatek projektu

---
+++ Projekt/test.js
@@ -0,0 +1,7 @@
+function x (){
+    var email=$("#inputEmail").val();
+    var passwd=$("#inputPassword").val();
+    
+}
+alert($("#form-signin").validate());
+$( "#submit-button" ).click(x);
--- lab6/10/main.js
+++ lab6/10/main.js
@@ -1,8 +1,12 @@
 
+// $( "#animate" ).click(function() {
+//   $("#asdf").animate({
+//     opacity: 0.25,
+//     left: "+=100"
+//   },1000
+//   );
+// });
+
 $( "#animate" ).click(function() {
-  $("#asdf").animate({
-    opacity: 0.25,
-    left: "+=50"
-  },1000
-  );
+  $( "#asdf" ).animate({ "left": "+=500px" },500 );
 });
