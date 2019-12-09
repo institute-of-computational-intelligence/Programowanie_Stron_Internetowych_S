aa5960b8b4aca814baba8779d850587856d1f2d5
PA <pa@test.com>
2019-12-09 17:42:17 +0100

    lab 8

---
+++ lab 8/scripts.js
@@ -0,0 +1,31 @@
+async function GetData1() {
+    var response = await fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json");
+    gridDataFetch = await response.json();
+    console.log("data downloaded by fetch");
+    console.log(gridDataFetch);
+}
+
+async function GetData2() { 
+    gridDataFetch = await $.ajax("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json");
+    console.log("data downloaded by fetch");
+    console.log(gridDataFetch);
+};
+  
+async function GetData3() {
+    gridDataFetch = await $.get("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json");
+    console.log("data downloaded by fetch");
+    console.log(gridDataFetch);
+}
+
+async function ShowData() {
+    gridDataFetch = await $.get("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json");
+    $('#table').bootstrapTable({
+        data: gridDataFetch
+    });
+}
+
+btn = document.getElementById("get-data-btn");
+btn.addEventListener("click", GetData1);
+btn.addEventListener("click", GetData2);
+btn.addEventListener("click", GetData3);
+btn.addEventListener("click", ShowData);
