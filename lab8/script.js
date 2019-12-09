f4ba8eac87319aa9ac833e0d93ae27c714057e77
Mateusz Wilk <mateuszwilk@qwe.pl>
2019-12-09 15:32:58 +0100

    Zadanie 10

--- lab8/script.js
+++ lab8/script.js
@@ -1,5 +1,8 @@
 var button = document.querySelector("#getData");
-var table = document.querySelector('table tbody')
+var table = document.querySelector('table tbody');
+
+var gridData;
+
 $.ajax({
     url: "https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json",
     type: "GET",
@@ -7,6 +10,9 @@ $.ajax({
   }).done(function(data) {
       
     $.each(data, function(i, obj) {
+
+        gridData = data;
+ 
         let el = "<th scope='row'>"+(i+1)+"</th>"+
                  "<td>"+obj.title+"</td>"+
                  "<td>"+obj.type+"</td>"+
@@ -22,3 +28,39 @@ $.ajax({
         
     });
   });
+
+
+$("#filter").click(function(){
+    var filterField = $("#filterField").val();
+
+    $.ajax({
+        url: "https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json",
+        type: "GET",
+        dataType: 'json'
+      }).done(function(data) {
+
+        $(table).empty();
+          
+        $.each(data, function(i, obj) {
+
+            if(obj.rating == filterField || filterField.length == 0) {
+                let el = "<th scope='row'>"+(i+1)+"</th>"+
+                "<td>"+obj.title+"</td>"+
+                "<td>"+obj.type+"</td>"+
+                "<td>"+obj.description+"</td>"+
+                "<td>"+obj.filename+"</td>"+
+                "<td>"+obj.height+"</td>"+
+                "<td>"+obj.width+"</td>"+
+                "<td>"+obj.price+"</td>"+
+                "<td>"+obj.rating+"</td>";
+            
+                $("<tr></tr>").html(el).appendTo(table);
+            }
+     
+
+           
+          
+            
+        });
+      });
+})
