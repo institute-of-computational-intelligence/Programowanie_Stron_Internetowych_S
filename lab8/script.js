24e09644d418456e0848e85d2d5aebaa47b1e0e3
Mateusz Wilk <mateuszwilk@qwe.pl>
2019-12-09 14:55:21 +0100

    Zadanie 5

---
+++ lab8/script.js
@@ -0,0 +1,11 @@
+$("#getData").click(function(){
+    fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json")
+    .then(response => {
+        response.json().then(json => {
+            gridDataFetch = json;
+            console.log("data downloaded by fetch");
+            console.log(gridDataFetch);
+        })
+    })
+    .catch((err) => console.log(err))
+})
