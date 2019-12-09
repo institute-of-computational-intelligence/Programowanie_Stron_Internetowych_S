f981ca614b4d6c7263cf8578f35a16207faf93f0
Mateusz Wilk <mateuszwilk@qwe.pl>
2019-12-09 15:02:20 +0100

    Zadanie 7

--- lab8/script.js
+++ lab8/script.js
@@ -1,7 +1,10 @@
 var button = document.querySelector("#getData");
 
 button.addEventListener('click', async () => {
-    const response = await fetch('https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json');
-    const myJson = await response.json();
-    console.log(JSON.stringify(myJson));
+    $.ajax({
+        url: "https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json",
+        type: "GET"
+      }).done(function(data) {
+        console.log(data);
+      });
 })
