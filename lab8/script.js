a365457deb051e2ff56568f9fea2cfc40c4e282b
Mateusz Wilk <mateuszwilk@qwe.pl>
2019-12-09 14:59:14 +0100

    Zadanie 6

--- lab8/script.js
+++ lab8/script.js
@@ -1,11 +1,7 @@
-$("#getData").click(function(){
-    fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json")
-    .then(response => {
-        response.json().then(json => {
-            gridDataFetch = json;
-            console.log("data downloaded by fetch");
-            console.log(gridDataFetch);
-        })
-    })
-    .catch((err) => console.log(err))
+var button = document.querySelector("#getData");
+
+button.addEventListener('click', async () => {
+    const response = await fetch('https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json');
+    const myJson = await response.json();
+    console.log(JSON.stringify(myJson));
 })
