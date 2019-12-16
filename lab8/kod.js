80c35f6dc4b033b8b82619654158f593e4a2c019
Krystian W <krycha@o2.pl>
2019-12-16 15:20:03 +0100

    lab 8

--- lab8/kod.js
+++ lab8/kod.js
@@ -1,3 +1,4 @@
+//zad5
 // $ (document).ready (function () {
 //   $ ('.btn.btn-primary').click (function () {
 //     fetch (
@@ -14,6 +15,7 @@
 //   });
 // });
 
+//zad6
 // $ (document).ready (function () {
 //   $ ('.btn.btn-primary').click (async function () {
 //     let response = await fetch (
@@ -25,12 +27,88 @@
 //   });
 // });
 
-$ (document).ready (function () {
-  $ ('.btn.btn-primary').click (async function () {
-    $.ajax ({
-      url: 'https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json',
-    }).done (function (z) {
-      console.log (z);
+//zad7
+// $(document).ready(function() {
+//   $('.btn.btn-primary').click(async function() {
+//     $.ajax({
+//       url:
+//         'https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json',
+//     }).done(function(z) {
+//       let data = JSON.parse(z);
+//       console.log(data);
+//     });
+//   });
+// });
+
+//zad8
+$(document).ready(function () {
+  $('.btn.btn-primary').click(async function () {
+    $.ajax({
+      url:
+        'https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json',
+    }).done(function (z) {
+      let data = JSON.parse(z);
+
+      $('tr:has(td)').remove();
+      // 2. get each article
+      $.each(data, function (index, article) {
+        // 2.2 Create table column for categories
+        var td_categories = $('<td/>');
+        article.index = index + 1;
+        // 2.3 get each category of this article
+        $.each(article.categories, function (i, category) {
+          var span = $('<span/>');
+          span.text(category);
+          td_categories.append(span);
+        });
+
+        // 2.4 Create table column for tags
+        var td_tags = $('<td/>');
+
+        // 2.5 get each tag of this article
+        $.each(article.tags, function (i, tag) {
+          var span = $('<span/>');
+          span.text(tag);
+          td_tags.append(span);
+        });
+
+        // 2.6 Create a new row and append 3 columns (title+url, categories, tags)
+        $('#added-articles').append(
+          $('<tr/>')
+            .append(
+              $('<td/>').html(
+                "<p='" + "'>" + article.index + '</p>'
+              )
+            )
+            .append(
+              $('<td/>').html(
+                "<p='" + "'>" + article.title + '</p>'
+              )
+            )
+            .append(
+              $('<td/>').html(
+                "<p='" + "'>" + article.type + '</p>'
+              )
+            )
+            .append(
+              $('<td/>').html(
+                "<p='" + "'>" + article.description + '</p>'
+              )
+            )
+            .append(
+              $('<td/>').html(
+                "<p='" + "'>" + article.filename + '</p>'
+              )
+            )
+            .append(
+              $('<td/>').html(
+                "<p='" + "'>" + article.height + '</p>'
+              )
+            )
+        );
+      });
+
+      console.log(data);
     });
   });
 });
