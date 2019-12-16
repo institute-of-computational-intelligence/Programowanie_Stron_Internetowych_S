613be53345fbcd15fead07c3a9b146e963796fbc
dziarski13 <takidajw@gmail.com>
2019-12-16 13:58:11 +0100

    lab8

--- lab08/script.js
+++ lab08/script.js
@@ -1,46 +1,121 @@
-var gridDataFetch = []
-document.getElementById("get-data-btn").addEventListener("click", async (sender) => {
-    var response = await fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json")
-    gridDataFetch = await response.json()
-    console.log("po deserializacji")
-    console.log(gridDataFetch)
+var gridDate2 = [];
+var gridDate3 = [];
 
-    Add();
+addEventListener('load', async(sender) => {
+    var response = await fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json")
+    gridDate2 = await response.json()
+    console.log(gridDate2)
+    add(gridDate2);
 });
 
-function Add() {
-    var table = document.getElementById("tableAdd");
-    for(var i = 0; i<gridDataFetch.length; i++){
+function add(gridDate) {
+    var element = document.getElementById("tableAdd");
+    element.innerHTML = "";
+    for (i = 0; i < gridDate.length; i++) {
         var row = document.createElement("tr");
-        
-        var index = document.createElement("th");
-        var col1 = document.createElement("td");
-        var col2 = document.createElement("td");
-        var col3 = document.createElement("td");
-        var col4 = document.createElement("td");
-        var col5 = document.createElement("td");
-        var col6 = document.createElement("td");
-        var col7 = document.createElement("td");
+        var number = document.createElement("th");
+        var col = [7];
+        var id = ['title', 'type', 'description', 'height', 'width', 'price', 'rating'];
+        for (j = 0; j < 7; j++) {
+            col[j] = document.createElement("td");
+            col[j].innerHTML = gridDate[i][id[j]];
+        }
+        number.innerHTML = i + 1;
 
-        index.innerHTML = i+1;
-        col1.innerHTML = gridDataFetch[i].title;
-        col2.innerHTML = gridDataFetch[i].type;
-        col3.innerHTML = gridDataFetch[i].description;
-        col4.innerHTML = gridDataFetch[i].height;
-        col5.innerHTML = gridDataFetch[i].width;
-        col6.innerHTML = gridDataFetch[i].price;
-        col7.innerHTML = gridDataFetch[i].rating;
-
-        row.appendChild(index);
-        row.appendChild(col1);
-        row.appendChild(col2);
-        row.appendChild(col3);
-        row.appendChild(col4);
-        row.appendChild(col5);
-        row.appendChild(col6);
-        row.appendChild(col7);
-
-        table.appendChild(row);
+        row.appendChild(number);
+        for (j = 0; j < id.length; j++) {
+            row.appendChild(col[j]);
+        }
+        element.appendChild(row);
     }
-    
+}
+
+document.getElementById("filter").addEventListener('input', filtrowanie);
+
+function filtrowanie() {
+    var list = document.getElementById("inputGroupSelect01");
+    var nubmerChoose = list.options[list.selectedIndex].value;
+    var value = filter.value.toLowerCase();
+    switch (nubmerChoose) {
+        case '1':
+            grid = gridDate2.filter(x => x.title.toLowerCase().includes(value));
+            break;
+
+        case '2':
+            grid = gridDate2.filter(x => x.title.toLowerCase().startsWith(value));
+            break;
+
+        case '3':
+            grid = gridDate2.filter(x => x.title.toLowerCase().endsWith(value));
+            break;
+    }
+    gridDate3 = grid;
+    add(grid);
+}
+
+var bool = true;
+
+function sort(n) {
+    if (gridDate3.length == 0) {
+        gridDate3 = gridDate2;
+    }
+    switch (n) {
+        case 1:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.title !== b.title ? a.title < b.title ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.title !== b.title ? a.title < b.title ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+        case 2:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.type !== b.type ? a.type < b.type ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.type !== b.type ? a.type < b.type ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+        case 3:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.description !== b.description ? a.description < b.description ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.description !== b.description ? a.description < b.description ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+        case 4:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.height !== b.height ? a.height < b.height ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.height !== b.height ? a.height < b.height ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+        case 5:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.width !== b.width ? a.width < b.width ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.width !== b.width ? a.width < b.width ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+        case 6:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.price !== b.price ? a.price < b.price ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.price !== b.price ? a.price < b.price ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+        case 7:
+            if (bool == true) {
+                var grid = gridDate3.sort((a, b) => a.rating !== b.rating ? a.rating < b.rating ? -1 : 1 : 0);
+            } else {
+                var grid = gridDate3.sort((a, b) => a.rating !== b.rating ? a.rating < b.rating ? 1 : -1 : 0);
+            }
+            add(grid);
+            break;
+    }
+    if (bool == true) { bool = false; } else { bool = true; }
 }
