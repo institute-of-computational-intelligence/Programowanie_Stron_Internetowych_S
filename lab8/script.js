00cd033a0feb9e79b53694c00d98c97008c7cfa8
Adrian_Gwizdzinski <adrian.gwizdzinski>
2019-12-16 15:21:15 +0100

    lab08 2

--- lab8/script.js
+++ lab8/script.js
@@ -1,4 +1,5 @@
 document.getElementById("getData").addEventListener("click", getDataFunction);
+document.getElementById("searchBtn").addEventListener("click", searchFunction);
 
 async function getDataFunction(){
     //fetch
@@ -16,12 +17,77 @@ async function getDataFunction(){
     try{
         let response = await fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json");
         let user = await response.json().then(json =>{
-            gridDataFetch = json
+            gridDataAwaitFetch = json
             console.log("data downloaded by fetch async")
-            console.log(gridDataFetch);
+            console.log(gridDataAwaitFetch);
         })
     }
     catch(err) { 
         console.error(err);
     }
+}
+
+//$.ajax
+$.ajax({
+    url : "https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json",
+    dataType : "json"
+})
+.done(json =>{
+    gridDatajQuery = json
+    console.log("data downloaded by jQuery")
+    console.log(gridDatajQuery)
+    var col = [];
+    for (var i = 0; i < gridDatajQuery.length; i++) {
+       for (var key in gridDatajQuery[i]) {
+           if (col.indexOf(key) === -1) {
+               col.push(key);
+            }
+        }
+    }
+    var table = document.getElementById("table");
+    var tr = table.insertRow(-1);
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    for (var i = 0; i < gridDatajQuery.length; i++) {
+        tr = table.insertRow(-1);
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = gridDatajQuery[i][col[j]];
+        }
+    }
+
+})
+
+async function searchFunction(){
+    $("#table").empty(); 
+    var a = document.getElementById("search").value;
+    var col = [];
+    arr1 = gridDatajQuery.filter(d => d.type == a)
+    for (var i = 0; i < arr1.length; i++) {
+       for (var key in arr1[i]) {
+           if (col.indexOf(key) === -1) {
+               col.push(key);
+            }
+        }
+    }
+    var table = document.getElementById("table");
+    var tr = table.insertRow(-1);
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    for (var i = 0; i < arr1.length; i++) {
+        tr = table.insertRow(-1);
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = arr1[i][col[j]];
+        }
+    }
+
 }
