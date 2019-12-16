dbf93b413d9426303e1986f73e697eef5f3d80fd
Mateusz Wilk <mateuszwilk@qwe.pl>
2019-12-16 14:21:58 +0100

    Zadanie 10

--- lab8/script.js
+++ lab8/script.js
@@ -8,59 +8,38 @@ $.ajax({
     type: "GET",
     dataType: 'json'
   }).done(function(data) {
-      
-    $.each(data, function(i, obj) {
 
-        gridData = data;
- 
-        let el = "<th scope='row'>"+(i+1)+"</th>"+
-                 "<td>"+obj.title+"</td>"+
-                 "<td>"+obj.type+"</td>"+
-                 "<td>"+obj.description+"</td>"+
-                 "<td>"+obj.filename+"</td>"+
-                 "<td>"+obj.height+"</td>"+
-                 "<td>"+obj.width+"</td>"+
-                 "<td>"+obj.price+"</td>"+
-                 "<td>"+obj.rating+"</td>";
-             
-        $("<tr></tr>").html(el).appendTo(table);
-      
-        
-    });
+    gridData = data;
+    draw(gridData);
+   
   });
 
 
 $("#filter").click(function(){
-    var filterField = $("#filterField").val();
+    var filtered = gridData.filter(function(single) {
+      return single.height == $("#filterField").val();
+    });
 
-    $.ajax({
-        url: "https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json",
-        type: "GET",
-        dataType: 'json'
-      }).done(function(data) {
+    draw(filtered);
 
-        $(table).empty();
-          
-        $.each(data, function(i, obj) {
-
-            if(obj.rating == filterField || filterField.length == 0) {
-                let el = "<th scope='row'>"+(i+1)+"</th>"+
-                "<td>"+obj.title+"</td>"+
-                "<td>"+obj.type+"</td>"+
-                "<td>"+obj.description+"</td>"+
-                "<td>"+obj.filename+"</td>"+
-                "<td>"+obj.height+"</td>"+
-                "<td>"+obj.width+"</td>"+
-                "<td>"+obj.price+"</td>"+
-                "<td>"+obj.rating+"</td>";
-            
-                $("<tr></tr>").html(el).appendTo(table);
-            }
-     
-
-           
-          
-            
-        });
-      });
 })
+
+function draw(data) {
+
+  $('table tbody').empty();
+
+  $.each(data, function(i, obj) {
+
+    let el = "<th scope='row'>"+(i+1)+"</th>"+
+              "<td>"+obj.title+"</td>"+
+              "<td>"+obj.type+"</td>"+
+              "<td>"+obj.description+"</td>"+
+              "<td>"+obj.filename+"</td>"+
+              "<td>"+obj.height+"</td>"+
+              "<td>"+obj.width+"</td>"+
+              "<td>"+obj.price+"</td>"+
+              "<td>"+obj.rating+"</td>";
+    $("<tr></tr>").html(el).appendTo('table tbody');
+  });
+
+}
