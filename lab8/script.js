a78dcf90511f50628aa2e69ee4adac835579f425
Bartłomiej_Wojtasinski <any@any.any>
2019-12-16 15:15:44 +0100

    dd

--- lab8/script.js
+++ lab8/script.js
@@ -1,65 +1,65 @@
 document.getElementById("getData").addEventListener("click", rzecz);
-document.getElementById("filter1").addEventListener("change", search(document.getElementById("filter1").value,document.getElementById("filter2").value));
-document.getElementById("filter2").addEventListener("change", search(document.getElementById("filter1").value,document.getElementById("filter2").value));
-//document.getElementById("filter").addEventListener("onchange", search(getElementById("filter")));
 
-window.onload = function() {
+
+window.onload = function()
+  {
     rzecz();
+    document.getElementById("filter1").addEventListener("keyup", search);
+    document.getElementById("filter2").addEventListener("keyup", search);
   };
+
+
 var trn=1;
 var table=document.getElementById("table");
 var dataset=[]; 
 async function rzecz()
 {
-   await fetch('https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json') 
-    .then(response => response.json()) 
-    .then(data => {
- 	dataset = data;
+    response= await fetch('https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json') ;
+    dataset=await response.json();
     logDataset(); 
-});
 };
 
 
-function logDataset () { 
-    
-    var filterValue="dairy";
-    
+
+function logDataset (filterValue) { 
     
     
-    for(i=0;i<dataset.length;i++)
-    {
-        if(dataset[i].type==="dairy")
+    
+    
+    console.log(filterValue);
+    
+        var tabTop="";
+        tabTop=Object.getOwnPropertyNames(dataset[0]);
+        var gg="";
+        for(var title of tabTop)
         {
-            var row=table.insertRow(trn);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-            var cell5 = row.insertCell(4);
-            var cell6 = row.insertCell(5);
-            var cell7 = row.insertCell(6);
-            var cell8 = row.insertCell(7);
-            cell1.innerHTML =dataset[i].title;
-            cell2.innerHTML =dataset[i].type;
-            cell3.innerHTML =dataset[i].description;
-            cell4.innerHTML =dataset[i].filename;
-            cell5.innerHTML =dataset[i].height;
-            cell6.innerHTML =dataset[i].width;
-            cell7.innerHTML =dataset[i].price;
-            cell8.innerHTML =dataset[i].rating;
-            trn++;
+            gg+=`<th scope="col">${title.toUpperCase()}</th>`;
         }
+     
+        document.getElementById("tableTop").innerHTML=gg;
+        var tbody="";
+        for(i=0;i<dataset.length;i++)
+        {
+        tbody+="<tr>"
+        if(dataset[i].type===filterValue )
+        {
+            
+            for(var value of tabTop)
+            {
+              tbody+=`<th >${dataset[i][value]}</th>` ;
+            }
+           
+        }
+        tbody+="</tr>";
    
     }
+    document.getElementById("tbody").innerHTML=tbody;
     
     console.log(dataset.length);
-    //console.table(dataset.title);
 }
-function search(x,y)
+function search(e)
 {
-   
+   logDataset(e.target.value);
 
-
-
-console.log(2);
+console.log(e.target.value);
 }
