e35b46896765d5abe84920be3e41a88922ac75bd
kosin.anna <aniakosin97@gmail.com>
2019-12-17 17:34:33 +0100

    lab_8_poprawione

--- Lab_8/script.js
+++ Lab_8/script.js
@@ -1,9 +1,10 @@
-var gridDataFetch=[]
-var gridDataFetchAsync=[]
+//var gridDataFetch=[]
+//var gridDataFetchAsync=[]
 var gridDatajQueryAjax=[]
-var getButton=document.querySelector("#getData");
+//var getButton=document.querySelector("#getData");
 
-getButton.addEventListener("click", async (sender)=>{
+//getButton.addEventListener("click", async (sender)=>{
+    /*
     //fetch promise
     fetch("https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json")
         .then(response =>{
@@ -25,20 +26,50 @@ getButton.addEventListener("click", async (sender)=>{
     catch(err){
         console.error(err)
     }
-
+*/
+async function cos(){
     //ajax jquery
     try{
-        gridDatajQueryAjax = await $.ajax({
+        var ajaxResponse = await $.ajax({
             url: "https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json",
             type: 'GET'
         });
+        var dataGrid = JSON.parse(ajaxResponse);
         console.log("data downloaded by ajax")
-        console.log(gridDatajQueryAjax)
+        console.log(dataGrid)
+        //var listOfKeys= Object.keys(dataGrid[0]);
+        dataGrid.forEach(function(element, index){
+            var tdTitle = $("<td>");
+            tdTitle.html(element.title);
+            var tdType = $("<td>");
+            tdType.html(element.type);
+            var tdDesc = $("<td>");
+            tdDesc.html(element.description);
+            var tdFileName = $("<td>");
+            tdFileName.html(element.filename);
+            var tdHeight = $("<td>");
+            tdHeight.html(element.height);
+            var tdWidth = $("<td>");
+            tdWidth.html(element.width);
+            var tdPrice = $("<td>");
+            tdPrice.html(element.price);
+            var tdRating = $("<td>");
+            tdRating.html(element.rating);
+
+            var tr = $("<tr>");
+            tr.append(tdTitle);
+            tr.append(tdType);
+            tr.append(tdDesc);
+            tr.append(tdFileName);
+            tr.append(tdHeight);
+            tr.append(tdWidth);
+            tr.append(tdPrice);
+            tr.append(tdRating); 
+            $("#tableB tbody").append(tr);
+        });
     }
     catch(err){
         console.error(err);
     }
-});
-while(gridDatajQueryAjax){
-
 }
+cos();
