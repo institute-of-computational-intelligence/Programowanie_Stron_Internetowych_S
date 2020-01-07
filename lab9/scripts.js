18bc8a780c90a7ebda5af60ee3c47f62a668cf6c
Łukasz_Kowalik <lukasz.kowalik41st@gmail.com>
2020-01-07 17:28:49 +0100

    zad 6-7

--- lab9/scripts.js
+++ lab9/scripts.js
@@ -5,6 +5,29 @@ $(function () {
         const surname = $('#last').val();
         const salary = $('#salary').val();
         let list = new Array(name, surname, salary);
-        console.log(list);
+        setItem('lista1', list, 'localStorage');
+        setItem('lista2', list, 'localStorage');
+        // setItem('lista3', list, 'localStorage');
+        // setItem('lista4', list, 'localStorage');
+        // setItem('lista5', list, 'sessionStorage');
+        let de = localStorage.getItem("lista1");
+        //  getItem('lista1', 'localStorage');
+        console.log(de);
     })
+
+    function setItem(name, value, storage) {
+        if (storage === 'localStorage') {
+            localStorage.setItem(name, value);
+        } else {
+            sessionStorage.setItem(name, value);
+        }
+    }
+
+    function getItem(name, storage) {
+        if (storage === 'localStorage') {
+            localStorage.getItem(name);
+        } else {
+            sessionStorage.getItem(name);
+        }
+    }
 });
