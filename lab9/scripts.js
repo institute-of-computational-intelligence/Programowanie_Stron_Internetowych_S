7adf819fb6eee95341d947960d5467b07b499e9b
Bartłomiej_Wojtasinski <any@any.any>
2020-01-13 15:08:54 +0100

    dsa

---
+++ lab9/scripts.js
@@ -0,0 +1,67 @@
+function save()
+{
+    var a=document.getElementById("fn").value;
+    var b=document.getElementById("sn").value;
+    var c=document.getElementById("sl").value;
+    
+    document.getElementById("x").innerHTML=
+    `<ul>
+    <li>${a}</li>
+    <li>${b}</li>
+    <li>${c}</li>
+    
+    </ul>`;
+}
+function setItem(name,value,storage)
+{
+   
+       storage[name]=value;
+    
+}
+
+function getItem(name,storage)
+{
+        return storage[name];
+}
+
+var request = window.indexedDB.open("MyTestDatabase", 3);
+
+idb.open('baza', 1);
+
+
+
+
+
+
+
+
+
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";               
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+
+
