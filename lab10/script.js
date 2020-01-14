c7e818ec19cdbbeb8c56b2fe6af3e005d4156ef1
Łukasz_Kowalik <lukasz.kowalik41st@gmail.com>
2020-01-14 17:31:16 +0100

    lab10 zad1-4

---
+++ lab10/script.js
@@ -0,0 +1,19 @@
+class Person {
+    constructor(firstName, lastName, dateofBirght) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateofBirght = dateofBirght;
+        this.fullName = firstName + " " + lastName;
+        this.age = new Date().getFullYear() - dateofBirght.getFullYear();
+    }
+}
+let viewModel = {
+    firstName: ko.observable(),
+    lastName: ko.observable(),
+    dataOfBirth: ko.observable(),
+    fullName: firstName + " " + lastName,
+    age: new Date().getFullYear() - dateofBirght.getFullYear()
+}
+let App = {
+    Persons: ko.
+}
--- lab9/scripts.js
+++ lab9/scripts.js
@@ -1,33 +1,32 @@
-$(function () {
-    $('#btn').click(function () {
+$(function() {
+  $("#btn").click(function() {
+    const name = $("#first").val();
+    const surname = $("#last").val();
+    const salary = $("#salary").val();
+    let list = new Array(name, surname, salary);
+    setItem("lista1", list, "localStorage");
+    setItem("lista2", list, "localStorage");
+    // setItem('lista3', list, 'localStorage');
+    // setItem('lista4', list, 'localStorage');
+    // setItem('lista5', list, 'sessionStorage');
+    let de = localStorage.getItem("lista1");
+    //  getItem('lista1', 'localStorage');
+    console.log(de);
+  });
 
-        const name = $('#first').val();
-        const surname = $('#last').val();
-        const salary = $('#salary').val();
-        let list = new Array(name, surname, salary);
-        setItem('lista1', list, 'localStorage');
-        setItem('lista2', list, 'localStorage');
-        // setItem('lista3', list, 'localStorage');
-        // setItem('lista4', list, 'localStorage');
-        // setItem('lista5', list, 'sessionStorage');
-        let de = localStorage.getItem("lista1");
-        //  getItem('lista1', 'localStorage');
-        console.log(de);
-    })
-
-    function setItem(name, value, storage) {
-        if (storage === 'localStorage') {
-            localStorage.setItem(name, value);
-        } else {
-            sessionStorage.setItem(name, value);
-        }
+  function setItem(name, value, storage) {
+    if (storage === "localStorage") {
+      localStorage.setItem(name, value);
+    } else {
+      sessionStorage.setItem(name, value);
     }
+  }
 
-    function getItem(name, storage) {
-        if (storage === 'localStorage') {
-            localStorage.getItem(name);
-        } else {
-            sessionStorage.getItem(name);
-        }
+  function getItem(name, storage) {
+    if (storage === "localStorage") {
+      localStorage.getItem(name);
+    } else {
+      sessionStorage.getItem(name);
     }
+  }
 });
