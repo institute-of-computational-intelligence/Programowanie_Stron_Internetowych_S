92dcafccc047933a99f6a382e3c80bae0153be1f
Giulia Katia Galimberti <example@gmail.com>
2020-01-20 17:34:04 +0100

    lab10 v2

--- Lab10/scripts.js
+++ Lab10/scripts.js
@@ -3,39 +3,54 @@ function PersonViewModel(person) {
 
   self.firstName = ko.observable(person.firstName);
   self.lastName = ko.observable(person.lastName);
-  self.fullName = ko.computed(function() {
+  self.fullName = ko.computed(function () {
     return self.firstName() + " " + self.lastName();
   });
-  self.dateOfBirth = ko.observable(person.dateOfBirth);
-  self.age = ko.computed(function() {
-    return (
-      new Date(Date.now()).getFullYear() -
-      new Date(person.dateOfBirth).getFullYear()
-    ).getM;
+  self.dateOfBirth = ko.observable(new Date(person.dateOfBirth));
+  self.age = ko.computed(function () {
+    return new Date().getFullYear() - person.dateOfBirth.getFullYear()
   });
 }
 
-var App = function(persons) {
-  var self = this;
-  self.persons = ko.observableArray(persons);
+var App = function (persons) {
+  this.persons = ko.observableArray(persons);
 
 
-  self.AddPerson = function(person) {
-      self.persons.push(person);
+  this.AddPerson = function (person) {
+    this.persons.push(person);
   }
 };
 
+let vm = [
+  new PersonViewModel({
+    firstName: "Marek",
+    lastName: "Bąk",
+    dateOfBirth: new Date("08-29-1997")
+  }),
+  new PersonViewModel({
+    firstName: "Mareczek",
+    lastName: "Roman",
+    dateOfBirth: new Date("01-01-2000")
+  })
+];
+
+let app = new App(vm)
 ko.applyBindings(
-  new App([
-    new PersonViewModel({
-      firstName: "Marek",
-      lastName: "Bąk",
-      dateOfBirth: new Date("08-29-1997")
-    }),
-    new PersonViewModel({
-      firstName: "Mareczek",
-      lastName: "Roman",
-      dateOfBirth: new Date("01-01-2000")
-    })
-  ])
+  app,
+  document.querySelector(".zad7")
 );
+
+let vm2 = new PersonViewModel({
+  firstName: "",
+  lastName: "",
+  dateOfBirth: new Date()
+})
+
+ko.applyBindings(
+  vm2,
+  document.querySelector(".zad8")
+);
+
+$("#btn1").click(() => {
+  app.persons.push(vm2)
+})
